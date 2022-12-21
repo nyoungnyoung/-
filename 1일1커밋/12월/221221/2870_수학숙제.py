e1407302f0351704dce3426ccde9479ecc319944
sunysa8 <sunysa8@naver.com>
2022-12-22 02:10:52 +0900

    2992, 2870

--- 1일1커밋/12월/221221/2870_수학숙제.py
+++
@@ -1,8 +0,0 @@
-# 숫자 + 알파벳 소문자 -> 숫자를 모두 찾아 비내림차순으로 정리
-# 숫자 앞에 0이 있는 경우 생략 가능
-import sys
-
-N = int(sys.stdin.readline().strip())
-for _ in range(N):
-    word = sys.stdin.readline().strip()
-    
